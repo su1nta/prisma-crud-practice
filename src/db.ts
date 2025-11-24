7c04fa050cff42e63da29ff9b35247a2888513c0
su1nta <sukanta.developer@gmail.com>
2025-11-24 19:07:03 +0530

    feat: addTodo logic

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,12 @@
 import { prisma } from "../lib/prisma.js";
 import { Prisma } from "../generated/prisma/client.js";
+import { StringFilter } from "../generated/prisma/commonInputTypes.js";
 
 export type UserType = Prisma.UsersCreateInput;
+type TodoType = Pick<
+    Prisma.TodosUncheckedCreateInput,
+    "title" | "priority" | "userId"
+>;
 
 export async function addUser(user: UserType) {
     try {
@@ -24,5 +29,21 @@ export async function getUsers() {
         return users;
     } catch (err) {
         console.log(err);
+    }
+}
+
+export async function addTodo(todo: TodoType) {
+    try {
+        const data: Prisma.TodosUncheckedCreateInput = {
+            title: todo.title,
+            userId: todo.userId,
+        };
+        if (todo.priority !== undefined) {
+            data.priority = todo.priority;
+        }
+        await prisma.todos.create({ data });
+        console.log("User created successfully");
+    } catch (err) {
+        console.log("Error: ", err);
     }
 }
